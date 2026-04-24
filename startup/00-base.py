a1cdc03fa3431e7c7bb2f016cff244f33d71486a
Max Rakitin <mrakitin@bnl.gov>
2026-04-24 16:49:05 -0400

    Fix 00-base

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,14 +1,5 @@
+import os
 import nslsii
-from bluesky import RunEngine
-nslsii.configure_base(
-    get_ipython().user_ns,
-    'chx',
-    redis_url = "xf11id1-chx-redis1.nsls2.bnl.gov",
-    redis_port = 6380,
-    redis_ssl = True,
-    publish_documents_with_kafka=False
-)
-
 import time
 from redis_json_dict import RedisJSONDict
 from tiled.client import from_profile
@@ -20,28 +11,14 @@ EpicsSignalBase.set_defaults(timeout=60, connection_timeout=60)  # new style
 from IPython import get_ipython
 from IPython.terminal.prompts import Prompts, Token
 
-class ProposalIDPrompt(Prompts):
-    def in_prompt_tokens(self, cli=None):
-        return [
-            (
-                Token.Prompt,
-                f"{RE.md.get('data_session', 'N/A')} [",
-            ),
-            (Token.PromptNum, str(self.shell.execution_count)),
-            (Token.Prompt, "]: "),
-        ]
-
-
-ip = get_ipython()
-ip.prompts = ProposalIDPrompt(ip)
-
-
 # Configure a Tiled writing client
 tiled_writing_client = from_profile("nsls2", api_key=os.environ["TILED_BLUESKY_WRITING_API_KEY_CHX"])["chx"]["raw"]
 
+
 class TiledInserter:
-    
-    name = 'chx'
+
+    name = "chx"
+
     def insert(self, name, doc):
         ATTEMPTS = 20
         error = None
@@ -58,12 +35,19 @@ class TiledInserter:
             # Out of attempts
             raise error
 
+
 tiled_inserter = TiledInserter()
 
 # The function below initializes RE and subscribes tiled_inserter to it
-nslsii.configure_base(get_ipython().user_ns,
-               tiled_inserter,
-               publish_documents_with_kafka=False,)
+nslsii.configure_base(
+    get_ipython().user_ns,
+    tiled_inserter,
+    redis_url="xf11id1-chx-redis1.nsls2.bnl.gov",
+    redis_port=6380,
+    redis_ssl=True,
+    publish_documents_with_kafka=False,
+)
+
 
 print("Initializing Tiled reading client...\nMake sure you check for duo push.")
 tiled_reading_client = from_profile("nsls2", username=None, include_data_sources=True)["chx"]["raw"]
@@ -71,8 +55,25 @@ tiled_reading_client = from_profile("nsls2", username=None, include_data_sources
 db = Broker(tiled_reading_client)
 
 # set plot properties for 4k monitors
-plt.rcParams['figure.dpi']=200
+plt.rcParams["figure.dpi"] = 200
+
 
 # Setup the path to the secure assets folder for the current proposal
 def assets_path():
     return f"/nsls2/data/chx/proposals/{RE.md['cycle']}/{RE.md['data_session']}/assets/"
+
+
+class ProposalIDPrompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        return [
+            (
+                Token.Prompt,
+                f"{RE.md.get('data_session', 'N/A')} [",
+            ),
+            (Token.PromptNum, str(self.shell.execution_count)),
+            (Token.Prompt, "]: "),
+        ]
+
+
+ip = get_ipython()
+ip.prompts = ProposalIDPrompt(ip)
